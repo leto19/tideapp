e03d9a13aa0fc451b12ec631ef592d40066a0d7a
George Close <closeg@cariff.ac.uk>
2018-07-24 01:21:14 +0100

    pulls and formats

--- tide.py
+++ tide.py
@@ -1,10 +1,33 @@
 import urllib2
+import re
+import calendar
 def pull():
     url = "https://www.tide-forecast.com/locations/Swansea-Wales/tides/latest"
     page = urllib2.urlopen(url)
-    content= page.read()
-    print(content)
+    content = page.readlines()
+    
+    return content
 
+def read(content):
+    out = []
     for line in content:
-        if 
-pull()
+        lineS = line.strip()
+        if any(ext in lineS for ext in list(calendar.day_name)) or '<td class="time tide">' in lineS or '<td class="tide">High Tide' in lineS or '<td class="tide">Low Tide'in lineS:
+            out.append(format(line))
+    return out
+
+
+
+
+def format(input_string):
+    out_string = re.sub(r'<.*?>', '', input_string)
+    return out_string.strip()
+
+def display(times):
+    for lines in times:
+        print(lines)
+
+display(read(pull()))
+
+
+
